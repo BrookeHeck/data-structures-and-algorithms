63d0d107de67160182c3492a6a3b97929fea3ec4
Brooke Heck <b19heck@gmail.com>
2022-10-01 01:28:29 +0000

    add stack tests

--- javascript/stack/index.js
+++ javascript/stack/index.js
@@ -9,37 +9,51 @@ class Node {
 
 class Stack {
   constructor() {
-    this.head = null;
+    this.top = null;
   }
 
   push(value) {
     const newNode = new Node(value);
     if(this.isEmpty()) {
-      this.head = newNode;
+      this.top = newNode;
     } else {
-      let temp = this.head;
-      this.head = newNode;
+      let temp = this.top;
+      this.top = newNode;
       newNode.next = temp;
     }
   }
 
   pop() {
     if(!this.isEmpty()) {
-      let currentTop = this.head;
-      this.head = this.head.next;
+      let currentTop = this.top;
+      this.top = currentTop.next;
       currentTop.next = null;
+      return currentTop.value;
     } else {
       throw new Error('Stack is empty');
     }
   }
 
   peek() {
-    return !this.isEmpty() ? this.head.value : new Error('Stack is empty');
+    if(!this.isEmpty()) return this.top.value;
+    else throw new Error('Stack is empty');
   }
 
   isEmpty() {
-    return !this.head;
+    return !this.top;
   }
+
+  toString() {
+    let string = 'TOP -> ';
+    let current = this.top;
+    while(current !== null) {
+      string = `${string}{ ${current.value} } -> `;
+      current = current.next;
+    }
+    string += 'NULL';
+    return string;
+  }
+
 }
 
 module.exports = Stack;
--- javascript/stack/stack.test.js
+++ javascript/stack/stack.test.js
@@ -1,7 +1,49 @@
 'use strict';
 
-describe('Test common stack methods', () => {
-  test('Should pass', () => {
-    expect(true).toBeTruthy();
+const Stack = require('./index');
+
+const stack = new Stack();
+
+describe('Test push and pop', () => {
+  test('Should insert a node at the head', () => {
+    stack.push(1);
+    expect(stack.top.value).toEqual(1);
   });
+
+  test('Should be able to add multiple nodes to the stack', () => {
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    let str = stack.toString();
+    expect(str).toEqual('TOP -> { 4 } -> { 3 } -> { 2 } -> { 1 } -> NULL');
+  });
+
+  test('Should pop off the top value', () => {
+    const top = stack.pop();
+    expect(top).toEqual(4);
+    expect(stack.top.value).toEqual(3);
+  });
+});
+
+describe('Test peek and isEmpty', () => {
+  test('Should return the top value', () => {
+    const value = stack.peek();
+    expect(value).toEqual(3);
+  });
+
+  test('Should return false when stack is not empty', () => {
+    const isEmpty = stack.isEmpty();
+    expect(isEmpty).not.toBeTruthy();
+  });
+
+  const empty = new Stack();
+  // test('Should throw an error when peek is called on an empty stack', () => {
+  //   expect(() => empty.peek()).toThrow('Stack is empty');
+  // });
+
+  test('Should return true when isEmpty is called on an empty stack', () => {
+    const isEmpty = empty.isEmpty();
+    expect(isEmpty).toBeTruthy();
+  });
+
 });
