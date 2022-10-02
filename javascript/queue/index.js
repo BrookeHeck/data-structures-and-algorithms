479b4e09ef378d4eac8a1585c74c1218c7ed4c86
Brooke Heck <b19heck@gmail.com>
2022-10-02 23:08:38 +0000

    add queue and tests

--- javascript/queue/index.js
+++ javascript/queue/index.js
@@ -3,6 +3,7 @@
 class Node {
   constructor(value) {
     this.value = value;
+    this.next = null;
   }
 }
 
@@ -13,25 +14,45 @@ class Queue {
   }
 
   enqueue(value) {
-
+    const newNode = new Node(value);
+    if(this.isEmpty()) {
+      this.front = newNode;
+    }
+    else if(!this.rear) {
+      this.rear = newNode;
+      this.front.next = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
   }
 
-  dequeue(value) {
-
+  dequeue() {
+    const temp = this.front;
+    this.front = this.front.next;
+    temp.next = null;
+    return temp;
   }
 
   peek() {
-
+    if(this.isEmpty()) throw new Error('Empty queue');
+    else return this.front.value;
   }
 
   isEmpty() {
-    return this.front === null & this.rear === null;
+    return this.front === null;
   }
 
   toString() {
-
+    let current = this.front;
+    let str = 'FRONT -> ';
+    while(current !== null) {
+      str = `${str}{ ${current.value} } -> `;
+      current = current.next;
+    }
+    str += 'REAR';
+    return str;
   }
-
 }
 
 module.exports = Queue;
