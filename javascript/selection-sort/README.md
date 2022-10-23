7edfaaf5a160daaf99da59968da8100c4181a460
Brooke Heck <b19heck@gmail.com>
2022-10-23 03:02:10 +0000

    add more tests

---
+++ javascript/selection-sort/README.md
@@ -0,0 +1,15 @@
+# Selection Sort
+
+Selection sort is an algorithm for sorting an array.
+
+## Stepping Through a Selection Sort
+
+## Pseudocode
+
+## An Example in JavaScript
+
+## Big O Notation
+
+## Resources
+[Selection Sort](https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Code_401/class-26/solutions/BLOG)
+
--- javascript/selection-sort/selection-sort.test.js
+++ javascript/selection-sort/selection-sort.test.js
@@ -7,11 +7,17 @@ describe('Test selection sort', () => {
     const testArr = [8, 4, 23, 42, 16, 15];
     selectionSort(testArr);
 
-    const testArr2 = [54, 10, 2, 1, 0, 100];
+    const testArr2 = [20, 18, 12, 8, 5, -2];
     selectionSort(testArr2);
 
     expect(testArr).toEqual([4, 8, 15, 16, 23, 42]);
-    expect(testArr2).toEqual([0, 1, 2, 10, 54, 100]);
+    expect(testArr2).toEqual([-2, 5, 8, 12, 18, 20]);
+  });
+
+  test('Test an array with only a few unique integers', () => {
+    const testArr = [5, 12, 7, 5, 5, 7];
+    selectionSort(testArr);
+    expect(testArr).toEqual([5, 5, 5, 7, 7, 12]);
   });
 
   test('Test an array that is empty', () => {
