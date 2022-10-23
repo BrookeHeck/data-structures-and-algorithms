367497de2af44bd87ac69f8b9acb109fd61c97d0
Brooke Heck <b19heck@gmail.com>
2022-10-23 03:48:11 +0000

    update readme

--- javascript/selection-sort/selection-sort.js
+++ javascript/selection-sort/selection-sort.js
@@ -4,7 +4,7 @@ function selectionSort(arr) {
   const n = arr.length;
   for(let i = 0; i < n; i++) {
     let min = i;
-    for(let j = (i + 1); j <= n; j++) {
+    for(let j = (i + 1); j < n; j++) {
       if(arr[j] < arr[min]) {
         min = j;
       }
