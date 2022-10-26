71b8378724417bfb22c86d43272fe77a28751a60
Brooke Heck <b19heck@gmail.com>
2022-10-26 21:51:12 +0000

    sort by year and title

--- javascript/movies/sort.js
+++ javascript/movies/sort.js
@@ -1,9 +1,17 @@
 function sortYear(movies) {
-  return [];
+  movies.sort((previous, current) => previous.year - current.year);
+  return movies;
 }
 
 function sortTitle(movies) {
-  return [];
+  movies.sort((previous, current) => {
+    const title1 = previous.title.replace('The ', '').toLowerCase();
+    const title2 = current.title.replace('The ', '').toLowerCase();
+    if(title1 < title2) return -1;
+    if(title1 > title2) return 1;
+    return 0;
+  });
+  return movies;
 }
 
 function inGenre(movies, genre) {
