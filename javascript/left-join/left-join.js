3ab45600b1b3fd161117a73e8cf7c8fd07a2e6e1
Brooke Heck <b19heck@gmail.com>
2022-11-03 00:08:14 +0000

    left join algorithm added

---
+++ javascript/left-join/left-join.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function leftJoin(left, right) {
+  const arr = [];
+  iterateKeys(left, (key) => {
+    let wordArr = [key, left.get(key)];
+    arr.push(wordArr);
+  });
+
+  iterateKeys(right, (key) => {
+    for(let wordArr of arr) {
+      if(wordArr[0] === key) {
+        wordArr.push(right.get(key));
+        break;
+      }
+    }
+  });
+}
+
+function iterateKeys(hashTable, callback) {
+  const keys = hashTable.keys();
+  for(let key of keys) {
+    callback(key);
+  }
+}
+
+module.exports = leftJoin;
