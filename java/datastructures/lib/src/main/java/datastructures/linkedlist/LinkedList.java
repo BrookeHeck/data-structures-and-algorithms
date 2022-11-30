e233c677ae241a713a64df8218448c08b0334d4f
BrookeHeck <b19heck@gmail.com>
2022-11-30 09:40:31 -0800

    start java data structures

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,7 +1,21 @@
 package datastructures.linkedlist;
 
+public class Node {
+  private String value;
+  public Node(value) {
+    this.value = String.parseString(value);
+  }
+}
+
 public class LinkedList
 {
+  private Node head;
+  public LinkedList() {
+    this.head = null;
+  }
+
+  
+
   @Override
   public String toString()
     {
