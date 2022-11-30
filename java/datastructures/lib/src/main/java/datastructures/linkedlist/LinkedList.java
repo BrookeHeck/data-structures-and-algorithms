5fccacfb0e86b8d1da2f5a5cc20731dbc7d369fe
BrookeHeck <b19heck@gmail.com>
2022-11-30 10:08:39 -0800

    start java linked list

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,24 +1,45 @@
-package datastructures.linkedlist;
+class Node {
+  public String value;
+  public Node next;
+  public Node(String value) {
+    this.value = value;
+    this.next = null;
+  }
 
-public class Node {
-  private String value;
-  public Node(value) {
-    this.value = String.parseString(value);
+  public void addNext(Node node) {
+    this.next = node;
   }
 }
 
-public class LinkedList
-{
+public class LinkedList {
   private Node head;
   public LinkedList() {
     this.head = null;
   }
 
-  
+  public void insert() {
+
+  }
+
+  public void append(String value) {
+    Node newNode = new Node(value);
+    Node current = this.head;
+    while(current.next != null) {
+      current = current.next;
+    }
+    current.addNext(newNode);
+  }
+
 
   @Override
-  public String toString()
-    {
-        return "null";  // TODO: implement me
+  public String toString() {
+    Node current = this.head;
+    String str = "HEAD -> ";
+    while(current != null) {
+      str += (current.value + " -> ");
+      current = current.next;
     }
+    str += "NULL";
+    return str;
+  }
 }
